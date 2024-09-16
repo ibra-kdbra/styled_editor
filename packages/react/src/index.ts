77499b81343899ede7a9d3258b131731ae451b6c
ibra-kdbra <git-repo-develop@outlook.com>
2024-09-16 12:19:00 +0300

    edit

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,6 @@
  * https://github.com/nytimes/react-prosemirror
  *
  * Former code:
- * https://github.com/BrianHung/editor-dev/commit/68b1dc3b4c6d373d752181ac4e0571dcda97e6e7
+ * https://github.com/ibra-kdbra/styled_editor-dev/commit/68b1dc3b4c6d373d752181ac4e0571dcda97e6e7
  */
 export * from '@nytimes/react-prosemirror';
