853149a5409481020fe5fc3410e2ff0a07775d9b
ibra-kdbra <git-repo-develop@outlook.com>
2024-09-16 12:27:34 +0300

    deploy model

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build:demo
+
+# navigate into the build output directory
+cd ghpages
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:<ibra-kdbra>/<ibra-kdbra>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+
+cd -
